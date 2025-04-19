1ac4c18ec784d6c85507ab528e8fa0c38d2d88bf
Tamer Jarmakani <tamerjar@hotmail.com>
2025-04-19 12:01:07 +0300

    New folder structure

---
+++ stravaSync/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    id:               { type: String, required: true, unique: true },
+    email:            { type: String, required: true, unique: true },
+    password:         { type: String, required: true },
+    name:             { type: String, required: true },
+    progress:         { type: String, default: "OKM To Bronze" },
+    totalDistance:    { type: String, default: "0KM" },
+    totalTime:        { type: String, default: "00:00:00" },
+    averagePace:      { type: String, default: "0:00" },
+    best5km:          { type: String, default: "00:00" },
+    best10km:         { type: String, default: "00:00" },
+    forcePasswordChange:{ type: Boolean, default: false },
+    isAdmin:          { type: Boolean, default: false },
+    stravaAccessToken:{ type: String },
+    stravaExpiresAt:  { type: Date },
+    stravaRefreshToken:{ type: String },
+    lastCronFetch:    { type: Date },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User ||
+  mongoose.model('User', userSchema);
